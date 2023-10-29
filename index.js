44db22dc19b3cba6467fe07a69b1f3dc89a2f1a4
valz6 <113355093+valz6@users.noreply.github.com>
2023-10-28 21:52:08 -0500

    Feat: Create the first part of CRUD

--- index.js
+++ index.js
@@ -3,14 +3,45 @@ const mysql = require("mysql");
 
 const app = express();
 
-const PORT = 3000;
- 
-app.listen(PORT, function(err){
-    if (err) console.log("Error in server setup")
-    console.log("Server listening on Port", PORT);
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+let db_conection = mysql.createConnection({
+    host: "localhost",
+    database: "juanjosuper",
+    user: "root",
+    password: ""
 })
+
+app.post("/validar", function(req, res){
+    console.log("Entré a validar");
+    const data = req.body;
+    console.log(data);
+    let identification = data.identification;
+    let name = data.name;
+    let email = data.email;
+    let password = data.password;
+    let role = data.role;
+
+    let register = `INSERT INTO tbl_usuario (tbl_usu_nombre,tbl_usu_correo,tbl_usu_contrasena,tbl_usu_cedula,tbl_usu_rol) VALUES ("${name}","${email}","${password}","${identification}","${role}")`;
+
+    db_conection.query(register, function(error){
+        if(error){
+            throw error;
+        }
+        else{
+            console.log("Datos almacenados con exito bajo el query "+register);
+        }
+    })
+
+});
 
 app.get("/", function(req, res){
     res.render("registro.ejs")
 })
-console.log("actualizado");
+
+app.listen(3000, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", "http://localhost:3000");
+})
